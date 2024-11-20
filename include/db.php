0a8089a38f7680e627868efae80ed3705674645b
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-11-20 09:33:59 +0100

    modified:   include/db.php

--- include/db.php
+++ include/db.php
@@ -1,20 +1,17 @@
 <?php
-// $servername = "localhost:3306"; 
-// $username = "mmchytli"; 
-// $password = "wAPEt1Igbekq"; 
-// $dbname = "mmchytli_mmcharity";
+
 ob_start();
 
 $servername = "localhost"; 
 $username = "root"; 
-$password = ""; 
-$dbname = "nemconsults";
+$password = getenv('SMTP_DB_PASS'); 
+$dbname = getenv('SMTP_DB_NAME');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ");
 }
 ?>

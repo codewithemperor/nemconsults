39b4ddbc1c67d3c7ff6ec9dafc4485af42d6f00c
Sulaimon Yusuf <codewithemperor@gmail.com>
2025-01-23 01:11:12 +0100

    modified:   include/functions.php

--- include/functions.php
+++ include/functions.php
@@ -1,5 +1,18 @@
 <?php
 include_once 'db.php'; 
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    $lines = file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) { // Skip comments
+            continue;
+        }
+        list($name, $value) = explode('=', $line, 2);
+        putenv(trim($name) . '=' . trim($value));
+    }
+} else {
+    die('.env file not found.');
+}
 
 // Function to add an blog
 function addBlog() {
